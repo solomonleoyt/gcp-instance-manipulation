25e0de7a9d4aa7f3536b3b5e7d33d629ffeed055
solomonleoyt <solomonleo.yt@gmail.com>
2024-09-23 06:45:56 +0000

    Added push script

---
+++ .ipynb_checkpoints/push_to_github-checkpoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to your project directory
+# cd /path/to/your/project
+
+# Add all changes
+git add .
+
+# Commit changes with a message
+git commit -m "$1"
+
+# Push changes to GitHub
+git push
